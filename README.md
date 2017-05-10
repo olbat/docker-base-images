7d188512f4696ae8bc5b2c562b5f49ce91d3b8d6
Luc Sarzyniec <devel@olbat.net>
2017-05-10 09:01:12 +0200

    Build Busybox, Alpine, Ubuntu, CentOS and Fedora images

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-# Debian base image builder [![Build Status](https://secure.travis-ci.org/olbat/docker-debian.png?branch=master)](https://travis-ci.org/olbat/docker-debian)
+# Docker base image builder [![Build Status](https://secure.travis-ci.org/olbat/docker-base-images.png?branch=master)](https://travis-ci.org/olbat/docker-base-images)
 
 
 ## Overview
-Build base Debian images using the official [mkimage.sh](https://github.com/docker/docker/blob/master/contrib/mkimage.sh) generation script from docker's upstream repository.
+Build base Docker  images using the official [mkimage.sh](https://github.com/docker/docker/blob/master/contrib/mkimage.sh) generation script from docker's upstream repository.
 
-The generated images are modified to setup an UTF-8 locale (see [Dockerfile](Dockerfile)).
+The generated images are modified to setup an UTF-8 locale when it's possible
+(see Dockerfiles).
 
 
 ## Dependencies
 The build script depends on the following softwares:
-* debootstrap
-* docker
 * bash
+* docker
+* busybox-static
+* debootstrap
+* rinse
 
 
 ## Usage
 ```
-usage: build.sh <repository> <release> [<tag1,tag2,...>]
+usage: build.sh <distrib> <release> [<repository> <tag1,tag2,...>]
 
 ```
-
-__Note__: the build can be customized specifying the `VARIANT` or `INCLUDE` environment variables
+__Note__: the build can be customized specifying the `INCLUDE` env. var.
 
 
 ## Example
@@ -28,7 +30,17 @@ Build and push the `user/debian` image based on the `stable` Debian release with
 
 ```bash
 # as root
-./build.sh user/debian stable jessie,8.x
+./build.sh debian user/debian stable jessie,8.x
 ```
 
 __Note__: if the image has to be pushed pushed to a login-enabled registry, you should first login using `docker login`
+
+
+## Images
+- [Busybox](https://hub.docker.com/r/olbat/busybox)
+- [Alpine](https://hub.docker.com/r/olbat/alpine)
+- [Debian](https://hub.docker.com/r/olbat/debian)
+- [Ubuntu](https://hub.docker.com/r/olbat/ubuntu)
+- [CentOS](https://hub.docker.com/r/olbat/centos)
+- [Fedora](https://hub.docker.com/r/olbat/fedora)
+__Note__: this images are generated on a daily basis using [Travis CI](https://travis-ci.org/olbat/docker-base-images)
--- build.sh
+++ build.sh
@@ -3,34 +3,77 @@
 set -e
 set -x
 
-INCLUDE=locales,${INCLUDE:-netcat}
-
 [ $# -lt 2 ] && (
-	echo "usage: $0 <repository> <release> [<tag1,tag2,...>]" 1>&2 ;
+	echo "usage: $0 <distrib> <release> [<repository> <tag1,tag2,...>]" 1>&2
 	exit 1
 )
 
 [ -d "docker/contrib" ] || (
-	echo "cannot read the docker/contrib directory, please make sure you "\
-		"initialized the docker/ git submodule properly" 1>&2
+	echo "ERROR: cannot read the docker/contrib directory, please make"\
+		"sure you initialized the docker/ git submodule properly" 1>&2
 	exit 1
 )
 
-mkdir -p ${BUILD_DIR:=build}
+mkdir -p ${BUILD_DIR:=$PWD/build}
+MKIMAGE_OPTS=${MKIMAGE_OPTS:--d $BUILD_DIR --compression=gz}
 
-docker/contrib/mkimage.sh -d $BUILD_DIR --compression=gz \
-	debootstrap --variant=${VARIANT:-minbase} --include=$INCLUDE $2
+case $1 in
+busybox)
+	[ $INCLUDE ] && echo "WARNING: $1 does not support \$INCLUDE" 1>&2
+	script=mkimage.sh
+	args="$MKIMAGE_OPTS busybox-static"
+	;;
 
-docker build -t $1:$2 .
-docker push $1:$2
+alpine)
+	[ $INCLUDE ] && echo "WARNING: $1 does not support \$INCLUDE" 1>&2
+	script=mkimage-alpine.sh
+	args="-s -r $2"
+	# FIXME: ugly hack since it's not possible to specify a build directory
+        #	 to the mkimage-alpine.sh script
+	postinstall="mv rootfs.tar.xz $BUILD_DIR"
+	;;
+
+debian|ubuntu)
+	[ $1 == ubuntu ] && components=main,universe || components=main
+
+	script=mkimage.sh
+	args="$MKIMAGE_OPTS debootstrap"
+	args+=" --variant=minbase --include=locales,${INCLUDE:-netcat}"
+	args+=" --components=$components $2"
+	;;
+
+centos|fedora|rhel|slc|opensuze)
+	pkgfile=$(mktemp)
+	trap 'rm -f -- "$pkgfile"' INT TERM HUP EXIT ERR
+	echo glibc-common >> $pkgfile
+	echo ${INCLUDE:-nmap-ncat} | tr "," "\n" >> $pkgfile
+
+	script=mkimage.sh
+	args="$MKIMAGE_OPTS rinse"
+	args+=" --distribution=$2 --add-pkg-list=$pkgfile"
+	;;
+
+*)
+	echo "invalid distrib $1" 1>&2
+	exit 1
+	;;
+esac
+
+docker/contrib/$script $args
+[ "$postinstall" ] && $postinstall
+
+image=${3:-$1}
+docker build -f Dockerfile.$1 -t $image .
 
 rm -rf $BUILD_DIR
 
-if [ $# -gt 2 ]
+if [ $# -gt 3 ]
 then
-	for tag in ${3//,/ }
+	for tag in ${4//,/ }
 	do
-		docker tag $1:$2 $1:$tag
-		docker push $1:$tag
+		docker tag $image $image:$tag
+		docker push $image:$tag
 	done
+else
+	docker push $image
 fi
